35cc2681c342d198b2654c310c58fb542cf28a69
boburhoshimov06 <boburhoshimov06@gmail.com>
2022-07-05 20:56:36 +0500

    home page text

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,7 +13,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: (Center(
-        child: Text("Hello, PDP Online and Academy"),
+        child: Text("Hello, PDP Online!"),
       )),
     );
   }
